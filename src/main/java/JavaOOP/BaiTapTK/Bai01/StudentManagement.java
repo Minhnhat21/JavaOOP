a556f61163c0244ad6492bfd5039a724dc3d78d9
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2021-11-12 19:48:29 +0700

    add my project

--- src/main/java/JavaOOP/BaiTapTK/Bai01/StudentManagement.java
+++ src/main/java/JavaOOP/BaiTapTK/Bai01/StudentManagement.java
@@ -26,11 +26,14 @@ public class StudentManagement {
         }
         System.out.println("Nhap nam sinh can tim kiem: ");
         int b_year = sc.nextInt();
-        for (Student student : studentlist) {
-            if(student.getBirth_year() == b_year) {
-                System.out.println(student);
-            }
-        }
+        studentlist.stream().filter(student -> (student.getBirth_year() == b_year)).forEachOrdered(student -> {
+            System.out.println(student);
+        });
+        System.out.println("Nhap ten sinh vien can tim kiem: ");
+        String i_name = sc.next();
+        studentlist.stream().filter(student -> (student.getName().equals(i_name))).forEachOrdered(student -> {
+            System.out.println(student);
+        });
 
     }
 }
